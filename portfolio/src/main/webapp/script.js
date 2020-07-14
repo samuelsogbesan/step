0fd66f1984341a2a71a5ce227ccaa135f5a4d647
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-14 13:01:46 +0000

    Add initial javascript for fullscreen.js

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,3 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
