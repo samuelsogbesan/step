17ef212cdf7dfeff6afb144ea0294c025233eb87
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-22 12:28:18 +0000

    Add Fetch function that outputs message to DOM

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,3 +11,16 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+function printMessageFromServer(){
+  fetch("/data")
+  .then(data => data.text())
+  .then(message => {
+    var container = document.getElementById("data-container");
+    var messageElement = document.createElement("h2");
+    messageElement.innerText = message;
+    container.appendChild(messageElement);
+  })
+}
+
+window.onload = () => document.getElementById("data-button").addEventListener("click",printMessageFromServer);
