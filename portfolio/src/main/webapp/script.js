6bff02650cc26319dbd83615aa0f0c18e41a3810
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-23 09:13:01 +0000

    Change loop to for-of

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,7 +16,7 @@ const navigate = (pageid) => document.getElementById(pageid).scrollIntoView({beh
 
 const initialiseNavigation = () => {
   var navigationItems = document.getElementsByClassName("navigation-item");
-  for(var i = 0; i <navigationItems.length;i++) navigationItems[i].addEventListener("click",function(){
+  for(const item of navigationItems) item.addEventListener("click",function(){
     navigate(this.getAttribute("data-link"));
   })
 }
