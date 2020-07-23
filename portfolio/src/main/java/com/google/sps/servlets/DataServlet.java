b36b89ecd5b81eb763baa0588ea12a0eaa6ef3e1
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-23 10:45:21 +0000

    Update json list definition

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,6 +17,7 @@ package com.google.sps.servlets;
 import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private ArrayList<String> json = Arrays.asList(new String[]{["Surprise!","Boo!","Woah!"]});
+  private List<String> json = Arrays.asList(new String[]{"Surprise!","Boo!","Woah!"});
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
