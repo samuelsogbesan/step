a0c479efcda4139de6d56eb22616e1cc666454ed
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-23 11:16:02 +0000

    Alphabetise import statements

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,10 +15,10 @@
 package com.google.sps.servlets;
 
 import com.google.gson.Gson;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
