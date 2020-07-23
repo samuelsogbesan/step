a9d214448d5f5970d929dabced67c3b12181f91f
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-23 10:41:02 +0000

    Update ArrayList Initialisation

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -27,7 +27,7 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private ArrayList<String> json = Arrays.asList(["Surprise!","Boo!","Woah!"]);
+  private ArrayList<String> json = Arrays.asList(new String[]{["Surprise!","Boo!","Woah!"]});
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
