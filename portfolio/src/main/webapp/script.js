4e28358113f8bb885ca931fd4050cecdabbe8c74
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-24 09:45:03 +0000

    CHange function signature

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-function printMessageFromServer() {
+const printMessageFromServer = () => {
   fetch('/data')
   .then(data => data.text())
   .then(message => {
