58a6beca50a7f2a1387096ae412343704e0c1aee
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-24 09:12:54 +0000

    Add space between strings

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -28,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private List<String> json = Arrays.asList("Surprise!","Boo!","Woah!");
+  private List<String> json = Arrays.asList("Surprise!", "Boo!", "Woah!");
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
