aedc5273b962d389a48e30cb56fc4a76f7d8d0e7
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-28 09:25:42 +0000

    Add Element Creator Helper

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+const create = (elementType, elementClassName) => {
+  const element = document.createElement(elementType);
+  element.className = elementClassName;
+  return element;
+}
+
 const addCommentElement = (comment) => {
   const el = document.createElement("div");
   el.className = "comment";
