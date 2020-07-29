f573d2fe2be791a841224ad0aa0dc648fb7247d0
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-07-29 08:44:03 +0000

    Move updateDatastore call to other function post method

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -66,6 +66,7 @@ public class DataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String nickname = getParameter(request, "nickname", "anon");
     String comment = getParameter(request, "comment", "");
+    updateDatastore(nickname, comment);
     List<FormData> formData = Arrays.asList(new FormData(nickname, comment));
     response.setContentType("application/json;");
     response.getWriter().println(new Gson().toJson(formData));
@@ -89,6 +90,6 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    updateDatastore("placeholder_nickname", "placeholder_comment");
+
   }
 }
