9e413b1c47e47f7eaf85da3866170e8b7b041825
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-07 11:58:03 +0000

    Change @param type from Request to MeetingRequest

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -21,7 +21,7 @@ public final class FindMeetingQuery {
     * Returns whether or not an event is attended by the required attendees from the Meeting Request
     *
     @param Event event The input event to compare against
-    @param Request request The subject meeting request
+    @param MeetingRequest request The subject meeting request
     @return a boolean value that is true if the event and request have overlapping attendees.
   */
   private static boolean isAttended(Event event, MeetingRequest request) {
