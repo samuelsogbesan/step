5c63b13c728d594fb4873dd63acf2ac0484e7cd1
Samuel Sogbesan <juwonsogbesan@gmail.com>
2020-08-10 08:34:29 +0000

    Update comments

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -41,20 +41,23 @@ public final class FindMeetingQuery {
  
     boolean[] occupiedTimes = getOccupiedTimes(arr, request);
  
-    int lowerBound = 0; // Start of Time Gap
-    int upperBound = 0; // End of Time Gap
+    int lowerBound = 0; // Start of Time Gap (first 0 past endPointer)
+    int upperBound = 0; // End of Time Gap (last 0 before first 1 after endpoint)
  
-    // Loop through array and find gaps between free slots. If gaps are sufficiently large, add them to list.
+    // Loop through array and find gaps between free slots.
+    // If gaps are sufficiently large, add them to list.
     for(int endPointer = 0; endPointer < occupiedTimes.length; endPointer++) {
       boolean slot = occupiedTimes[endPointer];
+
+      // If we find an occupied slot, set our gap upper bound to that slot.
       if(slot == true) upperBound = endPointer; //The last free slot between lowerBound and endPointer
       else if(endPointer == occupiedTimes.length-1) upperBound = endPointer+1; //Include bound in calculation. if the last value is an 0, we need to increase range to include it in difference
  
       if(slot == true || endPointer == occupiedTimes.length-1) {
-        int gapSize = upperBound - (lowerBound); 
+        int gapSize = upperBound - (lowerBound); // The range between these two values is the duration of the gap
         if(gapSize >= duration) times.add(TimeRange.fromStartEnd(getTime(lowerBound),getTime(upperBound),false));
  
-        //Fastforward lowerbound to next free space and move end pointer to count from there.
+        //Fastforward lowerbound to next free space and update end pointer to continue from there.
         for(lowerBound = endPointer; lowerBound < occupiedTimes.length && occupiedTimes[lowerBound] == true; lowerBound++);
         endPointer = lowerBound;
       }
