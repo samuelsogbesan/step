81bfdc64455246e261dbb0f21e319cd08fc14048
samuelsogbesan <47117318+samuelsogbesan@users.noreply.github.com>
2020-08-11 10:08:43 +0100

    Update walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
    Co-authored-by: Meike Baumgärtner <Somae@users.noreply.github.com>

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -33,7 +33,7 @@ public final class FindMeetingQuery {
   }
 
   /**
-    * Returns the minutes in a day from a half hour slot
+    * Returns the minutes in a day from a half hour slot.
     *
     @param int halfHour The halfhour slot to be converted.
     @exception IllegalArgumentException thrown if slot > 0 or slot > 47.
